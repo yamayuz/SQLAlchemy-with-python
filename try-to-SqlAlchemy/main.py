1d3fba561f60659f1bd4627eb386b2ea7afcb544
yamayuz <yuki@yamayulab.com>
2022-08-24 23:46:19 +0900

    [update] models separate to another files

--- try-to-SqlAlchemy/main.py
+++ try-to-SqlAlchemy/main.py
@@ -1,24 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import Column
-from sqlalchemy.types import Integer, String
+from models.User import User
 
 # DBエンジンの作成
 engine = create_engine("mysql+pymysql://root:root@mysql:3306/db?charset=utf8mb4")
-
-# モデルクラスの作成
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "user"
-    user_id = Column(Integer, primary_key=True)
-    first_name = Column(String(255))
-    last_name = Column(String(255))
-    age = Column(Integer)
-
-    def full_name(self):
-        return "{self.first_name} {self.last_name}"
 
 # テーブル作成(初回起動時のみ)
 #Base.metadata.create_all(bind=engine)
---
+++ try-to-SqlAlchemy/models/User.py
@@ -0,0 +1,13 @@
+from sqlalchemy.types import Integer, String
+from sqlalchemy.schema import Column
+from models.Base import Base
+
+class User(Base):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    age = Column(Integer)
+
+    def full_name(self):
+        return "{self.first_name} {self.last_name}"
