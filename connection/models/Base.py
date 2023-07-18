c48fba09374e6ebafce5315cfa3aea964238f6b1
yamayuz <yuki@yamayulab.com>
2023-07-18 14:09:24 +0900

    [change] modelのbaseを拡張させていたのをやめた

--- connection/models/Base.py
+++ connection/models/Base.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 
-class ModelBase(object):
 
-    @declared_attr
-    def __tablename__(cls):
-        return cls.__name__.lower()
-
-    __table_args__ = {'mysql_engine': 'InnoDB'}
+Base = declarative_base()
 
 
-Base = declarative_base(cls=ModelBase)
+# モデルのBaseを拡張する際に使用する
+# class ModelBase(object):
+#     @declared_attr
+#     def __tablename__(cls):
+#         return cls.__name__.lower()
+
+#     __table_args__ = {'mysql_engine': 'InnoDB'}
+
+
+# Base = declarative_base(cls=ModelBase)
